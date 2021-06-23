ace0eb4dea19f87c26d22eea1803231d45ce5e4e
Luke Rasmussen <luke.rasmussen@gmail.com>
2021-06-23 12:24:00 -0500

    Closes #31 - Filter project log

--- app/components/ProjectLog/ProjectLog.js
+++ app/components/ProjectLog/ProjectLog.js
@@ -1,6 +1,7 @@
 /* eslint-disable react/forbid-prop-types */
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 import DataTable from 'react-data-table-component';
 import Error from '../Error/Error';
 import ProjectLogRow from './ProjectLogRow/ProjectLogRow';
@@ -28,9 +29,71 @@ const columns = [
   }
 ];
 
+const TextField = styled.input`
+  height: 32px;
+  width: 200px;
+  border-radius: 3px;
+  border-top-left-radius: 5px;
+  border-bottom-left-radius: 5px;
+  border-top-right-radius: 0;
+  border-bottom-right-radius: 0;
+  border: 1px solid #e5e5e5;
+  padding: 0 32px 0 16px;
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+const ClearButton = styled.button`
+  border-top-left-radius: 0;
+  border-bottom-left-radius: 0;
+  border-top-right-radius: 5px;
+  border-bottom-right-radius: 5px;
+  height: 34px;
+  width: 32px;
+  text-align: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+// eslint-disable-next-line react/prop-types
+const FilterComponent = ({ filterText, onFilter, onClear }) => (
+  <>
+    <TextField
+      id="search"
+      type="text"
+      placeholder="Search logs"
+      aria-label="Search Input"
+      value={filterText}
+      onChange={onFilter}
+    />
+    <ClearButton type="button" onClick={onClear}>
+      X
+    </ClearButton>
+  </>
+);
+
 const projectLog = props => {
+  const [filterText, setFilterText] = React.useState('');
   const { feed, error } = props;
   const [pending, setPending] = useState(true);
+
+  const subHeaderComponentMemo = React.useMemo(() => {
+    const handleClear = () => {
+      if (filterText) {
+        setFilterText('');
+      }
+    };
+
+    return (
+      <FilterComponent
+        onFilter={e => setFilterText(e.target.value)}
+        onClear={handleClear}
+        filterText={filterText}
+      />
+    );
+  }, [filterText]);
 
   useEffect(() => {
     setPending(!props.feed && !props.error);
@@ -38,9 +101,17 @@ const projectLog = props => {
 
   let contents = <div className={styles.empty}>There are no actions or notifications to show</div>;
   if (feed) {
-    const data = feed.map(f => {
-      return { ...f, datetime: GeneralUtil.formatDateTime(f.timestamp) };
-    });
+    const data = feed
+      .map(f => {
+        return { ...f, datetime: GeneralUtil.formatDateTime(f.timestamp) };
+      })
+      .filter(
+        f =>
+          filterText === '' ||
+          (f.type && f.type.toLowerCase().includes(filterText.toLowerCase())) ||
+          (f.title && f.title.toLowerCase().includes(filterText.toLowerCase())) ||
+          (f.description && f.description.toLowerCase().includes(filterText.toLowerCase()))
+      );
     contents = (
       <DataTable
         title="Project Log"
@@ -50,6 +121,8 @@ const projectLog = props => {
         progressPending={pending}
         expandableRows
         expandableRowsComponent={<ProjectLogRow />}
+        subHeader
+        subHeaderComponent={subHeaderComponentMemo}
       />
     );
   } else if (error) {
