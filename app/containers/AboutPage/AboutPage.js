ccaa9e5217e5175d8f10e5dd5f8b87f0ae9e31f3
Luke Rasmussen <luke.rasmussen@gmail.com>
2022-10-12 18:52:16 -0500

    0.11

--- app/containers/AboutPage/AboutPage.js
+++ app/containers/AboutPage/AboutPage.js
@@ -5,7 +5,7 @@ export default class AboutPage extends Component {
   render() {
     return (
       <div className={styles.container} data-tid="container">
-        <h1>StatWrap v0.10</h1>
+        <h1>StatWrap v0.11</h1>
         <div className={styles.copyright}>
           (c) 2021-2022 Northwestern University Feinberg School of Medicine
         </div>
