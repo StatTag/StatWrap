f35e502188fefc6070f0367040bb89b37a884d82
Saksham Misra <122719607+vinfinity7@users.noreply.github.com>
2024-01-28 04:57:27 +0530

    Update Welcome.js

--- app/components/Welcome/Welcome.js
+++ app/components/Welcome/Welcome.js
@@ -6,9 +6,11 @@ const welcome = () => {
     <div className={styles.container}>
       <h1>Welcome to StatWrap</h1>
       <div className={styles.instructions}>
-        You can add a new project or load an existing project using the Projects list on the left
-        hand side. Select a project to load its information and start working with it. This text is
-        awful, I know. We&apos;ll come up with something better.
+        Welcome to our efficient project management platform. Whether you are initiating a new
+        project or revisiting an existing one, our user-friendly interface makes it seamless. Simply
+        select a project from the list on the left to access detailed information and kickstart your
+        tasks. Experience simplified project management with us, where productivity is just a click
+        away.
       </div>
     </div>
   );
