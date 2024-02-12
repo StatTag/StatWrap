8efae9302d7d2be1c9596112f638c2db97532c1d
Luke R <lrasmus@users.noreply.github.com>
2024-02-12 11:56:10 -0600

    Revert "(Fix) : Fixed [Webpack] configuration to handle .cs files"

--- configs/webpack.config.base.js
+++ configs/webpack.config.base.js
@@ -12,7 +12,7 @@ export default {
   module: {
     rules: [
       {
-        test: /\.(jsx?|cs)$/,
+        test: /\.jsx?$/,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
