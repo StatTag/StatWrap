35f2dcbc753af160be7b37ee1dc7c140c866507a
AdiAkhileshSingh15 <adiakhilesh.singh.cse21@itbhu.ac.in>
2024-03-15 02:15:26 +0530

    Changes tree-view rendering package from echarts-for-react to react-d3-tree

--- app/components/Workflow/DependencyTree/DependencyTreeEChart.js
+++ app/components/Workflow/DependencyTree/DependencyTreeEChart.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactECharts from 'echarts-for-react';
+import Tree from 'react-d3-tree';
 import WorkflowUtil from '../../../utils/workflow';
 import AssetUtil from '../../../utils/asset';
 import styles from './DependencyTree.css';
@@ -10,36 +10,31 @@ const dependencyGraphEChart = props => {
   const data = WorkflowUtil.getAllDependenciesAsTree(AssetUtil.filterIncludedFileAssets(assets));
   let tree = null;
   if (data) {
-    const option = {
-      tooltip: {
-        trigger: 'item',
-        triggerOn: 'mousemove'
-      },
-      series: [
-        {
-          type: 'tree',
-          id: 0,
-          name: 'tree1',
-          data: [data],
-          edgeShape: 'polyline',
-          edgeForkPosition: '63%',
-          initialTreeDepth: 2,
-          label: {
-            position: 'left',
-            align: 'right'
-          },
-          leaves: {
-            label: {
-              position: 'right',
-              align: 'left'
-            }
-          }
-        }
-      ]
-    };
-    tree = <ReactECharts option={option} style={{ height: '100%', width: '100%' }} />;
+    tree = (
+      <Tree
+        data={data}
+        initialDepth={2}
+        depthFactor={750}
+        separation={{
+          siblings: 0.4,
+          nonSiblings: 0.8,
+        }}
+        dimensions={{ height: window.innerHeight / 2, width: (2 * window.innerWidth) / 3 }}
+        pathFunc={'step'}
+        zoom={0.4}
+        scaleExtent={{ max: 2, min: 0.1 }}
+        translate={{ x: 10, y: window.innerHeight / 3 }}
+        rootNodeClassName={styles.node__root}
+        branchNodeClassName={styles.node__branch}
+        leafNodeClassName={styles.node__leaf}
+      />
+    );
   }
-  return <div className={styles.container}>{tree}</div>;
+  return (
+    <div style={{ width: '100%', height: '75vh', fontSize: '2.1em', fontFamily: 'monospace' }}>
+      {tree}
+    </div>
+  );
 };
 
 export default dependencyGraphEChart;
