8fbccd96f06b2748cc3d5cd47f775b8cf3d4311f
Luke R. <luke.rasmussen@gmail.com>
2025-03-04 16:57:28 -0600

    Cleanup and fix packaging
    Neglected to include our new worker files in the production build/package!

--- app/worker.js
+++ app/worker.js
@@ -1,7 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('Renderer loaded, requesting file content...');
   window.workerElectronBridge.listenForScanRequest((message) => {
-    console.log('Body of handling work');
-    console.log(message);
+    /* Placeholder for callback, if needed */
   });
 });
